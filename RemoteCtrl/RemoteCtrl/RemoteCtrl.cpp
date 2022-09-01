1b638c9fc3527102c03a3d0e4ff87cafd404d7a2
xingchenruoxi <158365021@qq.com>
2022-09-01 15:56:33 +0800

    1 完成功能：管理员运行检测 2 错误信息显示函数

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -18,9 +18,65 @@
 
 CWinApp theApp;
 using namespace std;
+//开机启动的时候，程序的权限是跟随启动用户的
+//如果两者权限不一致，则会导致程序启动失败
+//开机启动对环境变量有影响，如果依赖dll（动态库）,则可能启动失败
+// 解决方法：
+//【复制这些到system32下面或者sysWOW64下面】
+//system32下面，多是64位程序，sysWOW64下面，多是32位程序
+//【使用静态库，而非动态库】
 
-void ChooseAutoInvoke() {
+void WriteRefisterTable(const CString& strPath) {
 	CString strSubKey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+	char sPath[MAX_PATH] = "";
+	char sSys[MAX_PATH] = "";
+	std::string strExe = "\\RemoteCtrl.exe ";
+	GetCurrentDirectoryA(MAX_PATH, sPath);
+	GetSystemDirectoryA(sSys, sizeof(sSys));
+	std::string strCmd = "mklink " + std::string(sSys) + strExe + std::string(sPath) + strExe;
+	int ret = system(strCmd.c_str());
+	TRACE("ret = %d\r\n", ret);
+	HKEY hKey = NULL;
+	ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);
+	if (ret != ERROR_SUCCESS) {
+		RegCloseKey(hKey);
+		MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败！"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+		exit(0);
+	}
+	ret = RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength() * sizeof(TCHAR));
+	if (ret != ERROR_SUCCESS) {
+		RegCloseKey(hKey);
+		MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败！"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+		exit(0);
+	}
+	RegCloseKey(hKey);
+}
+
+/*
+* 改bug的思路
+* 0 观察现象
+* 1 先确定范围
+* 2 分析错误的可能性
+* 3 调试或者打日志，排查错误
+* 4 处理错误
+* 5 验证/长时间验证/多次验证/多条件的验证
+*/
+void WriteStarupDir(const CString& strPath) {
+	CString strCmd = GetCommandLine();
+	strCmd.Replace(_T("\""), _T(""));
+	BOOL ret = CopyFile(strCmd, strPath, FALSE);
+	if (ret == FALSE) {
+		MessageBox(NULL, _T("复制文件失败，是否权限不足？\r\n"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+		exit(0);
+	}
+}
+void ChooseAutoInvoke() {
+	TCHAR wcsSystem[MAX_PATH] = _T("");
+	//CString strPath = CString(_T("c:\\Windows\\SysWOW64\\RemoteCtrl.exe"));
+	CString strPath = _T("C\\Users\\xingchen\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup");
+	if (PathFileExists(strPath)) {
+		return;
+	}
 	CString strInfo = _T("该程序只允许合法的用途！");
 	strInfo += _T("继续运行该程序，将使得这台机器处于被监控状态！\n");
 	strInfo += _T("如果你不希望这样，请按”取消“按钮，退出程序。\n");
@@ -28,29 +84,8 @@ void ChooseAutoInvoke() {
 	strInfo += _T("按下”否“按钮，程序只运行一次，不会在系统内留下任何东西！\n");
 	int ret = MessageBox(NULL, strInfo, _T("警告"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
 	if (ret == IDYES) {
-		char sPath[MAX_PATH] = "";
-		char sSys[MAX_PATH] = "";
-		std::string strExe = "\\RemoteCtrl.exe ";
-		GetCurrentDirectoryA(MAX_PATH, sPath);
-		GetSystemDirectoryA(sSys, sizeof(sSys));
-		std::string strCmd = "cmd mklink" + std::string(sSys) + strExe + std::string(sPath) + strExe;
-		ret = system(strCmd.c_str());
-		TRACE("ret = %d\r\n", ret);
-		HKEY hKey = NULL;
-		ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);
-		if (ret != ERROR_SUCCESS) {
-			RegCloseKey(hKey);
-			MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败！"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
-			exit(0);
-		}
-		CString strPath = CString(_T("%SystemRoot%\\SysWOW64\\RemoteCtrl.exe"));
-		ret = RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength() * sizeof(TCHAR));
-		if (ret != ERROR_SUCCESS) {
-			RegCloseKey(hKey);
-			MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败！"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
-			exit(0);
-		}
-		RegCloseKey(hKey);
+		//WriteRefisterTable(strPath);
+		WriteStarupDir(strPath);
 	}
 	else if (ret == IDCANCEL) {
 		exit(0);
@@ -58,8 +93,46 @@ void ChooseAutoInvoke() {
 	return;
 }
 
+void ShowError() {
+	LPWSTR lpMessageBuf = NULL;
+	//strerror(errno);//标准c语言库
+	FormatMessage(
+		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+		NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPWSTR)&lpMessageBuf, 0, NULL);
+	OutputDebugString(lpMessageBuf);
+	LocalFree(lpMessageBuf);
+}
+
+bool IsAdmin() {
+	HANDLE hToken = NULL;
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+	{
+		ShowError();
+		return false;
+	}
+	TOKEN_ELEVATION eve;
+	DWORD len = 0;
+	if (GetTokenInformation(hToken, TokenElevation, &eve, sizeof(eve), &len) == FALSE) {
+		ShowError();
+		return false;
+	}
+	CloseHandle(hToken);
+	if (len == sizeof(eve)) {
+		return eve.TokenIsElevated;
+	}
+	printf("length of tokeninformation is %d\r\n", len);
+	return false;
+}
+
 int main()
 {
+	if (IsAdmin()) {
+		OutputDebugString(L"current is run as administrator!\r\n");
+	}
+	else {
+		OutputDebugString(L"current is run as normal user!");
+	}
 	int nRetCode = 0;
 
 	HMODULE hModule = ::GetModuleHandle(nullptr);
@@ -76,7 +149,7 @@ int main()
 		else
 		{
 			CCommand cmd;
-			ChooseAutoInvoke();
+			//ChooseAutoInvoke();
 			CServerSocket* pserver = CServerSocket::getInstance();
 			int ret = pserver->Run(&CCommand::RunCommand, &cmd);
 			switch (ret) {
